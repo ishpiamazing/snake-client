38f0545ff81c7f49a4ca450681e98af16c16a40c
ishpiamazing <ishpiamazing@gmail.com>
2020-08-25 17:57:53 +0000

    messages added to the file

--- client.js
+++ client.js
@@ -6,9 +6,18 @@ const net = require('net');
 const connect = function() {
   const conn = net.createConnection({ 
     host: 'localhost',
-    port: 50541
-    
+    port: 50541,
   });
+/*prints the message to the screen when connection is successfully established*/
+  conn.on('connect', () => {
+  console.log("Successfully connected to game server");
+  conn.write("Name: ISH");
+});
+
+
+
+
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('data', (data) => {
